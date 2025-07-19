4805b275f49dbd3383c04b0c7483a47d2724b4b7
uwuxverticuse <sagargusain150@gmail.com>
2025-07-19 20:48:45 +0530

    Linked List Example

---
+++ LinkedList/SinglyLinkedListExample.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class SinglyLinkedListExample {
+    public static void main(String [] args){
+        SinglyLinkedList myLinkedlist = new SinglyLinkedList();
+        myLinkedlist.insertFirst(16);
+        myLinkedlist.insertFirst(18);
+        myLinkedlist.insertFirst(19);
+        myLinkedlist.insertFirst(20);
+        myLinkedlist.insertLast(95);
+
+        myLinkedlist.display();
+
+    }
+}
